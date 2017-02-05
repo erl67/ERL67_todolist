d0e1df55d57dba616a78a734d2e88c1c2bff63da
E <E@erl>
2017-02-05 17:36:13 -0500

    Changed to border layout

--- ERL67_todolist/src/edu/pitt/todolist/view/View.java
+++ ERL67_todolist/src/edu/pitt/todolist/view/View.java
@@ -9,7 +9,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -26,21 +25,15 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.EtchedBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import edu.pitt.todolist.controller.AddButton;
-import edu.pitt.todolist.controller.DeleteButton;
 import edu.pitt.todolist.model.ListItem;
-import edu.pitt.todolist.model.Model;
 import edu.pitt.todolist.model.UserItem;
 
 public class View implements ListSelectionListener, ActionListener {
-
+	
 	private JFrame frmMain;
-	private JLabel lblToDo;
 	private JButton btnDelete;
 	private JButton btnAdd;
 	private JButton btnRst;
@@ -50,25 +43,35 @@ public class View implements ListSelectionListener, ActionListener {
 	private static JComboBox<UserItem> cboUser;
 	private static JComboBox<UserItem> cboFilter;
 	private JScrollPane list1scr;
-	private JPanel panel;
+	private JPanel panel = new JPanel();
+	private JPanel txtPanel = new JPanel();
+	private JPanel usrPanel = new JPanel();
+	private JPanel filterPanel = new JPanel();
+	private JPanel listPanel = new JPanel();
+	private JPanel btnPanel = new JPanel();
 
 	public View() {
-
+		
 		frmMain = new JFrame("IS1017 Assignment4 // ERL67");
-		frmMain.setLayout(new FlowLayout());
-		panel = new JPanel();
-		panel.setLayout(new FlowLayout());
-		JPanel txtPanel = new JPanel();
+		frmMain.setLayout(new BorderLayout());
+		panel.setLayout(new BorderLayout());		
 		txtPanel.setLayout(new BorderLayout(5,5));
-		JPanel listPanel = new JPanel();
-		listPanel.setLayout(new FlowLayout());
-		listPanel.setBackground(Color.GREEN);
-		listPanel.setBorder(BorderFactory.createRaisedBevelBorder());
-		JPanel btnPanel = new JPanel();
+		usrPanel.setLayout(new BorderLayout(5,5));
+		filterPanel.setLayout(new BorderLayout(5,5));
 		btnPanel.setLayout(new FlowLayout());
+		listPanel.setLayout(new FlowLayout());
+		
+		listPanel.setBackground(Color.DARK_GRAY);
 		txtPanel.setBackground(Color.DARK_GRAY);
+		usrPanel.setBackground(Color.DARK_GRAY);
 		btnPanel.setBackground(Color.DARK_GRAY);
+		filterPanel.setBackground(Color.DARK_GRAY);
 		panel.setBackground(Color.DARK_GRAY);
+		
+		listPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(0, Color.BLACK, Color.BLACK),  BorderFactory.createEtchedBorder(0, Color.LIGHT_GRAY, Color.BLACK)));
+		txtPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(0, Color.BLACK, Color.BLACK),  BorderFactory.createEtchedBorder(0, Color.LIGHT_GRAY, Color.BLACK)));
+		btnPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(1, Color.BLACK, Color.BLACK), BorderFactory.createEtchedBorder(0, Color.GREEN, Color.BLACK)));
+		filterPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(1, Color.BLACK, Color.BLACK), BorderFactory.createRaisedBevelBorder()));
 
 		frmMain.setContentPane(panel);
 		frmMain.setBounds(350, 150, 500, 575);
@@ -76,23 +79,40 @@ public class View implements ListSelectionListener, ActionListener {
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMain.setAlwaysOnTop(true);
 
-		lblToDo = new JLabel("To Do List");
+		JLabel lblToDo = new JLabel("To Do List");
 		lblToDo.setForeground(Color.RED);
 		lblToDo.setFont(new Font("Serif", Font.BOLD, 25));
 		lblToDo.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JLabel lblTask = new JLabel("Enter Task");
+		lblTask.setForeground(Color.GREEN);
+		lblTask.setFont(new Font("San Serif", Font.BOLD, 12));
+		
+		JLabel lblUser = new JLabel("Assign User");
+		lblUser.setForeground(Color.GREEN);
+		lblUser.setFont(new Font("San Serif", Font.BOLD, 12));
+		
+		JLabel lblFilter = new JLabel("Filter Users");
+		lblFilter.setForeground(Color.GREEN);
+		lblFilter.setFont(new Font("San Serif", Font.BOLD, 12));
 
-		jtfInput = new JTextField(40);
-		jtfInput.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+		jtfInput = new JTextField(30);
+		jtfInput.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createBevelBorder(1, Color.BLACK, Color.BLACK), BorderFactory.createEtchedBorder(1, Color.BLACK, Color.GREEN)));
 		jtfInput.setBackground(Color.WHITE);
-//		jtfInput.setText("     Enter Tasks Here");
-		jtfInput.setMargin(new Insets(10,10,10,10));
+		jtfInput.setPreferredSize(new Dimension(400,30));
+		
 		txtPanel.add(lblToDo, BorderLayout.PAGE_START);
+		txtPanel.add(lblTask, BorderLayout.WEST);
 		txtPanel.add(jtfInput, BorderLayout.CENTER);
 
 		cboUser = new JComboBox<UserItem>();
 		cboUser.setBackground(Color.lightGray);
 		cboUser.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder()));
-		txtPanel.add(cboUser, BorderLayout.PAGE_END);
+		cboUser.insertItemAt(null, 0);
+		
+		usrPanel.add(lblUser, BorderLayout.WEST);
+		usrPanel.add(cboUser, BorderLayout.CENTER);
+		txtPanel.add(usrPanel, BorderLayout.PAGE_END);	
 		
 		listModel = new DefaultListModel<ListItem>();
 		list1 = new JList<ListItem>(listModel);
@@ -100,79 +120,66 @@ public class View implements ListSelectionListener, ActionListener {
 		list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list1.addListSelectionListener(this);
 		list1.setVisibleRowCount(20);
-		//list1.setPreferredSize(new Dimension(300,300));
 		list1scr = new JScrollPane(list1, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		listPanel.add(list1scr);
 
 		btnDelete = new JButton("Remove Item");
 		btnAdd = new JButton("Add Item");
-		btnRst = new JButton("Reset");
+		btnRst = new JButton("Reset View");
 		btnAdd.setPreferredSize(new Dimension(120, 40));
 		btnDelete.setPreferredSize(new Dimension(120, 40));
 		btnRst.setPreferredSize(new Dimension(120, 40));
 		btnPanel.add(btnAdd); btnPanel.add(btnDelete); btnPanel.add(btnRst);
 
-		cboFilter = new JComboBox<UserItem>();
-		cboFilter.setPreferredSize(new Dimension(300,20));
-
-		panel.add(txtPanel);
-		panel.add(listPanel);
-		panel.add(btnPanel);
-		panel.add(cboFilter);
+		cboFilter = new JComboBox<UserItem>();	
+		cboFilter.insertItemAt(null, 0);
+		filterPanel.add(lblFilter, BorderLayout.WEST);
+		filterPanel.add(cboFilter, BorderLayout.CENTER);
+		
+		panel.add(txtPanel, BorderLayout.PAGE_START);
+		listPanel.add(btnPanel);
+		panel.add(listPanel, BorderLayout.CENTER);
+		panel.add(filterPanel, BorderLayout.PAGE_END);
 		frmMain.setVisible(true);
-
-		ActionListener addListener = new AddButton(null);
-		btnAdd.addActionListener(addListener);
-
-		ActionListener deleteListener = new DeleteButton(null);
-		btnDelete.addActionListener(deleteListener);
-
-		btnRst.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				Model.resetButton();
-			}
-		});
-
-		cboFilter.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				Model.filterUsers((UserItem)cboFilter.getSelectedItem());
-			}
-		});
-
 	}
-
-	public static void fillComboBox(){
-		cboFilter.addItem(new UserItem (-1, " ", "    SELECT HERE TO FILTER BY USER"));
-		for (UserItem u: Model.getUserList().values()){
-			cboUser.addItem(u);
-			cboFilter.addItem(u);
-		}
+	
+	public static JComboBox<UserItem> getCboUser() {
+		return cboUser;
+	}
+	
+	public static JComboBox<UserItem> getCboFilter() {
+		return cboFilter;
 	}
 
 	public static UserItem cboUserSelect() {
 		return (UserItem) cboUser.getSelectedItem();
+	}
+	
+	public static UserItem cboFilterSelect() {
+		return (UserItem) cboFilter.getSelectedItem();
 	}
 
 	public static JList<ListItem> getList1() {
 		return list1;
 	}
 
-	public static DefaultListModel<ListItem> getListModel() {
+	public static  DefaultListModel<ListItem> getListModel() {
 		return listModel;
-	}
-
-	public static void setListModel(DefaultListModel<ListItem> listModel) {
-		View.listModel = listModel;
 	}
 
 	public static JTextField getJtfInput() {
 		return jtfInput;
 	}
-
-	public static void resetJtfInput() {
-		jtfInput.setText(null);
+	
+	public JButton getDeleteButton() {
+		return btnDelete;
+	}
+	
+	public JButton getResetButton() {
+		return btnRst;
+	}
+	public JButton getAddButton() {
+		return btnAdd;
 	}
 
 	@Override
@@ -182,5 +189,4 @@ public class View implements ListSelectionListener, ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 	}
-
 }
